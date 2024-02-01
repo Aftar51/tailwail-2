bc1890c74ecaa8042e1d46776e90201962a3f09b
aftar51 <afftardarda@gmail.com>
2024-02-01 09:41:29 +0700

    js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ module.exports = {
 ],
   theme: {
     fontFamily: {
-    'poppins' : ['Poppins']
+    'poppins' : ['Poppins'],
+
   },
-  extend: {},
+  extend: {
+    colors: {
+      primaryColor: '#010a5e',
+      primaryColorLight: '#010d78',
+      secondaryColor: '#FFCC00',
+      paragraphColor: '#c0c0c0',
+      whiteColor: '#fff',
+      blackColor: '#000',
+      greenColor: '#007936',
+      redColor: '#cc3433',
+      darkColor: '#000',
+      darkColorLight: '#171717',
+    }
+  },
   },
   plugins: [
     require('flowbite/plugin')
