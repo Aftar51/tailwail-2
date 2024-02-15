dec2587b09e3829be2b596a068c868be8a5fe950
aftar51 <afftardarda@gmail.com>
2024-02-15 13:55:30 +0700

    NEW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
-],
-darkMode: 'class',
-theme: {
-    fontFamily: {
-    'poppins' : ['Poppins'],  
-  },
-  extend: {
-    colors: {
-      primaryColor: '#010a5e',
-      primaryColorLight: '#010d78',
-      secondaryColor: '#FFCC00',
-      paragraphColor: '#c0c0c0',
-      whiteColor: '#fff',
-      blackColor: '#000',
-      greenColor: '#007936',
-      redColor: '#cc3433',
-      darkColor: '#000',
-      darkColorLight: '#171717',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        "sm":"480px"
+      }
     },
-    keyframes: {
-      move: {
-        "50%": {transform: 'translateY(-1rem)'}
-      },
-      neon: {
-        "0%": {
-          textShadow: "0 0 10px #00B9FC",
-        },
-        "100%": {
-          textShadow: "0 0 10px #ff0000",
-        },
-      },
-    },
-    animation: {
-      'movingY': 'move 2s linear infinite'
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
     }
   },
-  container: {
-    center: true,
-    padding: {
-      DEFAULT: '1rem',
-      sm: '1.5rem'
-    }
-  },
-  screen:{
-    sm: '480px',
-    md: '768px',
-    lg: '1024px'
-  },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [],
+} 
 
