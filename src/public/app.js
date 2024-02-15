fa0e9a52bb5a02711d882c8dfd571b02ffafdabf
aftar51 <afftardarda@gmail.com>
2024-02-15 20:57:49 +0700

    reset

--- src/public/app.js
+++ src/public/app.js
@@ -6,3 +6,16 @@ hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("bg-white")
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            navbar.classList.add('bg-white');
+            navbar.classList.remove('bg-transparent');
+        } else {
+            navbar.classList.remove('bg-white');
+            navbar.classList.add('bg-transparent');
+        }
+    });
+});
